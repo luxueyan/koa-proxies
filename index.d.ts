aa448c5e0a4b57afc55097c81d43f96c01146b08
luxueyan <luxueyan@kaitongamc.com>
2020-08-11 17:49:41 +0800

    style: update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,16 @@
-import * as Koa from 'koa';
+import * as Koa from 'koa'
 
-declare function KoaProxies(path: string, options: KoaProxies.IKoaProxiesOptions): Koa.Middleware;
+declare function KoaProxies(path: string, options: KoaProxies.IKoaProxiesOptions): Koa.Middleware
 
 declare namespace KoaProxies {
   interface IKoaProxiesOptions {
-    target: string;
-    changeOrigin?: boolean;
-    logs?: boolean | ((ctx: Koa.Context, target: string) => void);
-    agent?: any;
-    rewrite?: (path: string) => string;
+    target: string
+    changeOrigin?: boolean
+    logs?: boolean | ((ctx: Koa.Context, target: string) => void)
+    agent?: any
+    router?: Record<string, string> | ((req: Koa.Request) => string)
+    rewrite?: (path: string) => string
   }
 }
 
-export = KoaProxies;
+export = KoaProxies
